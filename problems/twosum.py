548e4f59e7848d685201347f5fc2e855aab9d743
wildgrandma <chinturocks2001@gmail.com>
2023-01-24 22:38:10 +0530

    just minor changes

--- problems/twosum.py
+++ problems/twosum.py
@@ -4,9 +4,16 @@
 nums = [2,7,11,15]
 target = 9
 
-def twosum(self,nums:list[int],target:int)->list[int]:
-    # brute force
-    l = {}
-    n = len(nums)
-    for num in enumerate(nums):
+class Sol:
+    def twosum(nums: list[int],target: int) -> list[int]:
+        seen = {}
+        for i, value in enumerate(nums): #1
+           remaining = target - nums[i] #2
+           
+           if remaining in seen: #3
+               return [i, seen[remaining]]  #4
+           else:
+               seen[value] = i  #5
+
+    def brute(nums:list[int],target:int) -> list[int]:
         
